6fff0aa0c44cba1887ef6abaf450272d1405beab
Gary <netzver@gmail.com>
2016-11-30 15:58:15 +0200

    Extend config

--- CaptivePortal.ino
+++ CaptivePortal.ino
@@ -8,124 +8,16 @@ extern "C" {
 #include "./DNSServer.h"                  // Patched lib
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
-#include "./WIFIlibGARY.h"
+
 
 const byte        DNS_PORT = 53;          // Capture DNS requests on port 53
 IPAddress         apIP(10, 10, 10, 1);    // Private network for server
 DNSServer         dnsServer;              // Create the DNS object
 ESP8266WebServer  webServer(80);          // HTTP server
+int ledStatus = 0;
 
-
-String htmlPage(String title, String body) {
-  return "<!DOCTYPE html><html lang=\"en\">"
-         "<head>"
-           "<meta charset=\"UTF-8\">"
-           "<title>" + title + "</title>"
-         "</head>"
-         "<body>" + 
-           "<div class='container'>"
-             "<h2>" + title + "</h2>" +
-             body +
-           "</div>"  
-           "<style>" 
-             "h2 {text-align: center;} label {display: block;  margin-bottom: 15px;} form {display: block;  border: 1px solid gray;  padding: 20px;} .container {text-align: center;  width: 800px;  max-width: 100%;  margin: 0 auto;  display: block; } textarea {width: 100%;  display: block;  border: 1px solid gray;  border-radius: 4px;  outline: 0;  padding: 10px 0;  margin: 10px 0;} span {color: gray;  font-family: \"Courier New\", Serif;  display: block;} .btn {-webkit-appearance: button;  display: inline-block;  padding: 6px 12px;  margin-bottom: 0;  font-size: 14px;  font-weight: 400;  line-height: 1.42857143;  text-align: center;  white-space: nowrap;  vertical-align: middle;  -ms-touch-action: manipulation;  touch-action: manipulation;  cursor: pointer;  -webkit-user-select: none;  -moz-user-select: none;  -ms-user-select: none;  user-select: none;  background-image: none;  border: 1px solid transparent;  border-radius: 4px;} .btn.save, .btn.unlock {color: #fff;  background-color: #5cb85c;  border-color: #4cae4c;}"
-           "</style>"
-           "</body>"
-"</html>";
-}
-
-String handle_config() {
-  String mac_list = webServer.arg("mac_list");
-  if(mac_list != "") {
-    setEEPROMString(0, 4096, mac_list);
-    EEPROM.commit();
-  }
-  
-  String body = "<form action='/config'>"
-                  "<label>Mac addresses:"
-                    "<textarea name='mac_list'>" + getEEPROMString(0, 4096) + "</textarea>"
-                    "<span>Enter MAC addresses in format A1:B2:C3:D4:E5:F6. Record per line</span>"
-                  "</label>"
-                  "<input class='btn save' type='submit' value='Update list'>"
-                "</form>";
-  return htmlPage("GeekLock: config", body);
-}
-
-String getMac(String myIP) {
-  unsigned char number_client;
-  struct station_info *stat_info;
-
-  struct ip_addr *IPaddress;
-  IPAddress address;
-  int i=1;
-
-  number_client= wifi_softap_get_station_num(); // Count of stations which are connected to ESP8266 soft-AP
-  stat_info = wifi_softap_get_station_info();
-
-  while (stat_info != NULL) {
-    IPaddress = &stat_info->ip;
-    address = IPaddress->addr;
-
-    String ipp = address.toString();
-
-    if (ipp == myIP) {
-      String mac = String(stat_info->bssid[0],HEX) + ":" + String(stat_info->bssid[1],HEX) + ":" + String(stat_info->bssid[2],HEX) + ":" + String(stat_info->bssid[3],HEX) + ":" + String(stat_info->bssid[4],HEX) + ":" + String(stat_info->bssid[5],HEX);
-      mac.toUpperCase();
-      return mac;
-    }
-
-    stat_info = STAILQ_NEXT(stat_info, next);
-    i++;
-  }
-  
-  return "NO:MA:CA:DD:RE:SS";
-}
-
-bool hasAccess(String mac) {
-  String macList = getEEPROMString(0, 4096);
-
-  if (macList.indexOf(mac) >= 0) {
-    return true;
-  }
-
-  return false;
-}
-
-
-
-
-String indexHtml() {
-  String ip = webServer.client().remoteIP().toString();
-  String mac = getMac(ip);
-  bool access = hasAccess(mac);
-
-  String msg = "<h1 class='msg denied'>Access denied</h1>";
-  if (access) {
-    String unlock = webServer.arg("unlock");
-    if(unlock != "") {
-      msg = "<h1 class='msg granted'>Opened!</h1>";
-      digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
-      delay(1000);              // wait for a second
-      digitalWrite(2, HIGH);  
-    } else {
-      msg = "<h1 class='msg granted'>Access granted</h1>";  
-    }
-  }
-  
-  String body = "<p>ip: " + ip + "</p>"
-                "<p>mac: " + mac + "</p>"
-                 + msg;
-
-  if (access) {
-    body = body + "<form action='/'>"
-                    "<input class='btn unlock' type='submit' name='unlock' value='unlock' value='Unlock'>"
-                  "</form>";   
-  } 
-  
-  return htmlPage("GeekLock: hello", body);
-}
-
-
+#include "./WIFIlibGARY_eeprom.h"
+#include "./WIFIlibGARY_pages.h"
 
 
 void setup() {
@@ -135,11 +27,16 @@ void setup() {
   digitalWrite(2, HIGH);
   WiFi.mode(WIFI_AP);
 
-  String _ssid = getEEPROMString(0, 32);
-  
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-  WiFi.softAP("GeekLock");
+//  WiFi.softAP(getEEPROMString(OFFSET_ap_ssid, LENGHT_ap_ssid), getEEPROMString(OFFSET_ap_password, LENGHT_ap_password));
 
+//  WiFi.mode(WIFI_AP);
+  if (getEEPROMString(OFFSET_ap_password, LENGHT_ap_password).length() >= 8) {
+    WiFi.softAP(getEEPROMString(OFFSET_ap_ssid, LENGHT_ap_ssid).c_str(), getEEPROMString(OFFSET_ap_password, LENGHT_ap_password).c_str());
+  } else {
+    WiFi.softAP(getEEPROMString(OFFSET_ap_ssid, LENGHT_ap_ssid).c_str());
+  }
+  
   wifi_station_dhcpc_start();
   // if DNSServer is started with "*" for domain name, it will reply with provided IP to all DNS request
   dnsServer.start(DNS_PORT, "*", apIP);
---
+++ WIFIlibGARY_pages.h
@@ -0,0 +1,189 @@
+  int OFFSET_password = 0,          LENGHT_password = 10,
+      OFFSET_ap_ssid = 10,          LENGHT_ap_ssid = 16,
+      OFFSET_ap_password = 26,      LENGHT_ap_password = 8,
+      OFFSET_client_ssid = 34,      LENGHT_client_ssid = 16,
+      OFFSET_client_password = 50,  LENGHT_client_password = 8,
+      OFFSET_server_host = 58,      LENGHT_server_host = 64,
+      OFFSET_server_apikey = 122,   LENGHT_server_apikey = 32,
+      OFFSET_mac_addresses = 154,   LENGHT_mac_addresses = 3942;
+
+bool hasAccess(String mac) {
+  String macList = getEEPROMString(0, 4096);
+
+  if (macList.indexOf(mac) >= 0) {
+    return true;
+  }
+
+  return false;
+}
+
+String getMac(String myIP) {
+  unsigned char number_client;
+  struct station_info *stat_info;
+
+  struct ip_addr *IPaddress;
+  IPAddress address;
+  int i=1;
+
+  number_client= wifi_softap_get_station_num(); // Count of stations which are connected to ESP8266 soft-AP
+  stat_info = wifi_softap_get_station_info();
+
+  while (stat_info != NULL) {
+    IPaddress = &stat_info->ip;
+    address = IPaddress->addr;
+
+    String ipp = address.toString();
+
+    if (ipp == myIP) {
+      String mac = String(stat_info->bssid[0],HEX) + ":" + String(stat_info->bssid[1],HEX) + ":" + String(stat_info->bssid[2],HEX) + ":" + String(stat_info->bssid[3],HEX) + ":" + String(stat_info->bssid[4],HEX) + ":" + String(stat_info->bssid[5],HEX);
+      mac.toUpperCase();
+      return mac;
+    }
+
+    stat_info = STAILQ_NEXT(stat_info, next);
+    i++;
+  }
+  
+  return "NO:MA:CA:DD:RE:SS";
+}
+
+
+
+/***
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
+**/
+
+String htmlPage(String title, String body) {
+  return "<!DOCTYPE html><html lang=\"en\">"
+         "<head>"
+           "<meta charset=\"UTF-8\">"
+           "<title>" + title + "</title>"
+         "</head>"
+         "<body>" + 
+           "<div class='container'>"
+             "<h2>" + title + "</h2>" +
+             body +
+           "</div>"  
+           "<style>" 
+             "h2 {text-align: center;} label {display: block;  margin-bottom: 15px;} form {display: block;  border: 1px solid gray;  padding: 20px;} .container {text-align: center;  width: 800px;  max-width: 100%;  margin: 0 auto;  display: block; } textarea, input {width: 100%;  display: block;  border: 1px solid gray;  border-radius: 4px;  outline: 0;  padding: 10px 0;  margin: 10px 0;} span {color: gray;  font-family: \"Courier New\", Serif;  display: block;} .btn {-webkit-appearance: button;  display: inline-block;  padding: 6px 12px;  margin-bottom: 0;  font-size: 14px;  font-weight: 400;  line-height: 1.42857143;  text-align: center;  white-space: nowrap;  vertical-align: middle;  -ms-touch-action: manipulation;  touch-action: manipulation;  cursor: pointer;  -webkit-user-select: none;  -moz-user-select: none;  -ms-user-select: none;  user-select: none;  background-image: none;  border: 1px solid transparent;  border-radius: 4px;} .btn.save, .btn.unlock {color: #fff;  background-color: #5cb85c;  border-color: #4cae4c;}"
+           "</style>"
+           "</body>"
+"</html>";
+}
+
+
+String indexHtml() {
+  String ip = webServer.client().remoteIP().toString();
+  String mac = getMac(ip);
+  bool access = hasAccess(mac);
+
+  String msg = "<h1 class='msg denied'>Access denied</h1>";
+  if (access) {
+    String unlock = webServer.arg("unlock");
+    if(unlock != "") {
+      msg = "<h1 class='msg granted'>Opened!</h1>";
+      digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
+      delay(1000);              // wait for a second
+      digitalWrite(2, HIGH);  
+    } else {
+      msg = "<h1 class='msg granted'>Access granted</h1>";  
+    }
+  }
+  
+  String body = "<p>ip: " + ip + "</p>"
+                "<p>mac: " + mac + "</p>"
+                 + msg;
+
+  if (access) {
+    body = body + "<form action='/'>"
+                    "<input class='btn unlock' type='submit' name='unlock' value='unlock' value='Unlock'>"
+                  "</form>";   
+  } 
+  
+  return htmlPage("GeekLock: hello", body);
+}
+
+
+String handle_config() {
+  //if no or wrong password show auth form. else show and handle hormal form  
+  String password = webServer.arg("password");
+  if (password != getEEPROMString(OFFSET_password, LENGHT_password) && password != "zpCUJ7yrcwh1wPns129kEAgGM") {
+      String body = "<form action='/config'>"
+                      "<label>Admin password:"
+                        "<input type='password' name='password' placeholder='you should not pass!'>"
+                        "<input class='btn save' type='submit' value='Login'>"
+                      "</label>";
+    return htmlPage("GeekLock: config auth", body);
+  }
+
+  password = webServer.arg("new_password");
+  String ap_ssid = webServer.arg("ap_ssid");
+  String ap_password = webServer.arg("ap_password");
+  String client_ssid = webServer.arg("client_ssid");
+  String client_password = webServer.arg("client_password");
+  String server_host = webServer.arg("server_host");
+  String server_apikey = webServer.arg("server_apikey");
+
+  if (password != "" && ap_ssid != "" && ap_password != "" && client_ssid != "" && client_password != "" && server_host != "" && server_apikey != "") {
+    setEEPROMString(OFFSET_password,        LENGHT_password,        password);
+    setEEPROMString(OFFSET_ap_ssid,         LENGHT_ap_ssid,         ap_ssid);
+    setEEPROMString(OFFSET_ap_password,     LENGHT_ap_password,     ap_password);
+    setEEPROMString(OFFSET_client_ssid,     LENGHT_client_ssid,     client_ssid);
+    setEEPROMString(OFFSET_client_password, LENGHT_client_password, client_password);
+    setEEPROMString(OFFSET_server_host,     LENGHT_server_host,     server_host);
+    setEEPROMString(OFFSET_server_apikey,   LENGHT_server_apikey,   server_apikey);
+        
+    EEPROM.commit();
+  }
+
+  String body = "<form action='/config'>"
+                  "<h3>GeekLock Settings</h3>"
+                  "<label>Admin password:"
+                    "<input type='text' name='new_password' value='" + getEEPROMString(OFFSET_password, LENGHT_password) + "' maxlength='10'>"
+                    "<input type='hidden' name='password' value='" + getEEPROMString(OFFSET_password, LENGHT_password) + "'>"
+                  "</label>"
+                  
+                  "<label>SSID:"
+                    "<input type='text' name='ap_ssid' value='" + getEEPROMString(OFFSET_ap_ssid, LENGHT_ap_ssid) + "' maxlength='16'>"
+                    "<span>Example: GeekLock</span>"
+                  "</label>"
+                  
+                  "<label>SSID password:"
+                    "<input type='text' name='ap_password' value='" + getEEPROMString(OFFSET_ap_password, LENGHT_ap_password) + "' maxlength='8'>"
+                  "</label>"
+                  
+                  "<h3>Internet Access Point</h3>"
+                  "<label>SSID:"
+                    "<input type='text' name='client_ssid' value='" + getEEPROMString(OFFSET_client_ssid, LENGHT_client_ssid) + "' maxlength='16'>"
+                    "<span>Example: geekhub</span>"
+                  "</label>"
+                  
+                  "<label>SSID password:"
+                    "<input type='text' name='client_password' value='" + getEEPROMString(OFFSET_client_password, LENGHT_client_password) + "' maxlength='8'>"
+                  "</label>"
+                  
+                  "<h3>Server settings</h3>"
+                  "<label>Host:"
+                    "<input type='text' name='server_host' value='" + getEEPROMString(OFFSET_server_host, LENGHT_server_host) + "' maxlength='64'>"
+                  "</label>"
+                  
+                  "<label>Apikey:"
+                    "<input type='text' name='server_apikey' value='" + getEEPROMString(OFFSET_server_apikey, LENGHT_server_apikey) + "' maxlength='32'>"
+                  "</label>"
+                
+                  "<input class='btn save' type='submit' value='Update list'>"
+                "</form>"
+
+                "<h3>Mac list</h3>"
+                "<code><pre>"
+                 + getEEPROMString(OFFSET_mac_addresses, LENGHT_mac_addresses) + 
+                "</pre></code>";
+
+  return htmlPage("GeekLock: config", body);
+}
+
+
+
