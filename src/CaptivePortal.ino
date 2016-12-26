935ad6a298ace55ae79d6c5b8b95d30cd25ccb74
Gary <netzver@gmail.com>
2016-12-27 01:31:22 +0200

    add leds, auto reconnect

--- src/CaptivePortal.ino
+++ src/CaptivePortal.ino
@@ -6,6 +6,9 @@ extern "C" {
 }
 #endif
 
+#define GREEN_LED_PIN 4
+#define RED_LED_PIN 5
+
 #include <ESP8266WiFi.h>
 #include "./DNSServer.h"                  // Patched lib
 #include <ESP8266WebServer.h>
@@ -13,17 +16,17 @@ extern "C" {
 #include <EEPROM.h>
 #include <ArduinoJson.h>
 
-
 const byte        DNS_PORT = 53;          // Capture DNS requests on port 53
 IPAddress         apIP(10, 10, 10, 1);    // Private network for server
 DNSServer         dnsServer;              // Create the DNS object
 ESP8266WebServer  webServer(80);          // HTTP server
 
-unsigned long previousMillis = 0, currentMillis = 0; // last time update
+unsigned long previousMillis = 0, currentMillis = 0, lockAtMillis = 0; // last time update
 long INTERVAL_status = 30000; // interval at which to do something (milliseconds)
+long INTERVAL_lock = 10000;
 
 long timestamp,
-     timestamp_update_time; 
+     timestamp_update_time;
 
 String apikey;
 
@@ -31,22 +34,14 @@ typedef struct {
   String mac;
   String key;
 } user_struct;
-
 user_struct users[100];
 
 typedef struct {
   user_struct user;
   long timestamp;
 } logrecord;
-
 logrecord queue[25];
 int queue_length = 0;
-
-
-////TODO test more than 25 queue lenght
-
-
-
 
 #include "./WIFIlibGARY_eeprom.h"
 #include "./WIFIlibGARY_pages.h"
@@ -55,8 +50,11 @@ int queue_length = 0;
 void setup() {
   EEPROM.begin(4096);
   Serial.begin(115200);
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  digitalWrite(GREEN_LED_PIN, LOW);
+  digitalWrite(RED_LED_PIN, LOW);
+
   WiFi.mode(WIFI_AP_STA);
 
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
@@ -67,6 +65,20 @@ void setup() {
   } else {
     WiFi.softAP(getEEPROMString(OFFSET_ap_ssid, LENGHT_ap_ssid).c_str());
   }
+
+  Serial.println("Config data");
+  Serial.print("SSID: ");
+  Serial.print(getEEPROMString(OFFSET_ap_ssid, LENGHT_ap_ssid));
+  Serial.print("  password: ");
+  Serial.println(getEEPROMString(OFFSET_ap_password, LENGHT_ap_password));
+
+  Serial.print("Inet SSID: ");
+  Serial.print(getEEPROMString(OFFSET_client_ssid, LENGHT_client_ssid));
+  Serial.print("  password: ");
+  Serial.println(getEEPROMString(OFFSET_client_password, LENGHT_client_password));
+
+  Serial.print("Admin password: ");
+  Serial.print(getEEPROMString(OFFSET_password, LENGHT_password));
 
   //connect to wifi
   WiFi.begin(getEEPROMString(OFFSET_client_ssid, LENGHT_client_ssid).c_str(), getEEPROMString(OFFSET_client_password, LENGHT_client_password).c_str());
@@ -120,19 +132,38 @@ void loop() {
   currentMillis = millis();
 
   if(currentMillis - previousMillis > INTERVAL_status || previousMillis > currentMillis) {
-    //previousMillis > currentMillis: It just show the elapsed time from the start of the board. This number will overflow (go back to zero), after approximately 50 days (source: arduino.cc/en/Reference/millis) 
-     previousMillis = currentMillis;  
+    //previousMillis > currentMillis: It just show the elapsed time from the start of the board. This number will overflow (go back to zero), after approximately 50 days (source: arduino.cc/en/Reference/millis)
+     previousMillis = currentMillis;
 
      getStatus();
      pushQueue();
+
+     if (WiFi.status() != WL_CONNECTED) {
+       Serial.println("Not connected, try to reconnect");
+       WiFi.begin(getEEPROMString(OFFSET_client_ssid, LENGHT_client_ssid).c_str(), getEEPROMString(OFFSET_client_password, LENGHT_client_password).c_str());
+     } else {
+       Serial.println("Ok, connected");
+     }
+  }
+
+  if(lockAtMillis && lockAtMillis < currentMillis) {
+    //do lock after 10 seconds
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    lockAtMillis = 0;
   }
 }
 
-void pushQueue() {
+void pushQueue() { //push log to server
   Serial.println("pushQueue()");
+  int queue_array_length = sizeof(queue) /sizeof(queue[0]);
+  int total = queue_length;
+  if (total > queue_array_length) {
+    total = queue_array_length;
+  }
 
   Serial.println("== QUEUE ==");
-  for (int i = 0; i < queue_length; i++) {
+  for (int i = 0; i < total; i++) {
        Serial.print((i+1));
        Serial.print(" user: [");
        Serial.print(queue[i].user.mac);
@@ -142,13 +173,12 @@ void pushQueue() {
        Serial.println(queue[i].timestamp);
   }
 
-  if (queue_length) {
-    
+  if (total) {
     DynamicJsonBuffer jsonBuffer;
     JsonObject& root = jsonBuffer.createObject();
     JsonArray& queueArr = root.createNestedArray("queue");
-    
-    for (int i = 0; i < queue_length; i++) {
+
+    for (int i = 0; i < total; i++) {
       JsonObject& record = queueArr.createNestedObject();
       record["mac"] = queue[i].user.mac;
       record["key"] = queue[i].user.key;
@@ -157,7 +187,7 @@ void pushQueue() {
     root.prettyPrintTo(Serial);
     char buffer[1000];
     root.printTo(buffer, sizeof(buffer));
-  
+
     HTTPClient http;
     http.begin("http://garik.pp.ua/prj/geeklock/access-log/");
     http.addHeader("apikey", apikey);
@@ -168,14 +198,12 @@ void pushQueue() {
     http.writeToStream(&Serial);
     Serial.println(http.getString());
     http.end();
-  
-  
-  } 
+  }
 }
 
 void getStatus() {
   Serial.println("getStatus()");
-  
+
   HTTPClient http;
   StaticJsonBuffer<200> jsonBuffer;
 
@@ -183,11 +211,10 @@ void getStatus() {
   http.addHeader("apikey", apikey);
   int httpCode = http.GET();
   String json = "[empty]";
-  if(httpCode>0){ 
+  if(httpCode>0){
     json = http.getString();
     Serial.println(json);
 
-    //TODO parse json here
     JsonObject& root = jsonBuffer.parseObject(json);
     if (!root.success()) {
       Serial.println("parseObject() failed");
@@ -195,18 +222,18 @@ void getStatus() {
     }
 
     timestamp = root["timestamp"].as<long>();
-    timestamp_update_time = millis();    
+    timestamp_update_time = millis();
     const long db_update = root["db_update"].as<long>();
     const long open      = root["open"].as<long>();
     const bool lock      = root["lock"];
 
     if (EEPROMReadLong(OFFSET_db_timestamp) != db_update) {
       Serial.println("OLD DB, UPPDATE!!! ");
-      getUsers();  
+      getUsers();
       EEPROMWriteLong(OFFSET_db_timestamp, db_update);
       EEPROM.commit();
     }
-   
+
     //open lock from server
     if (EEPROMReadLong(OFFSET_open_timestamp) != open) {
       Serial.println("OPEN DOOR - SERVER COMMAND ");
@@ -215,23 +242,29 @@ void getStatus() {
       EEPROM.commit();
     }
 
-    //TODO do global lock (disable unlock, until server unable it again. server only.)
+    //---TODO do global lock (disable unlock, until server unable it again. server only.)
 
     //TODO do ESP.reset(); on save config
 
-    //TODO add led status
+    //+++TODO add led status
 
     //TODO print QR-code with SSID and IP address of this geeklock
 
-    http.end();  
+
+    //TODO if door opened physicaly more than 30 seconds do alert, do beep
+
+    //TODO beeper
+
+    //swagger.io
+
+    http.end();
   } else {
     Serial.print("HTTP code ");
     Serial.println(httpCode);
   }
 }
 
-
-void loadUsers() {
+void loadUsers() { //load from EEPROM
   int users_array_length = sizeof(users) /sizeof(users[0]);
   for (int i = 0; i < users_array_length; i++) {
     users[i].mac = getEEPROMString(OFFSET_users + i * (mac_length + key_length),              mac_length);;
@@ -247,22 +280,21 @@ void loadUsers() {
     Serial.print(users[i].mac);
     Serial.print(", KEY = ");
     Serial.println(users[i].key);
-  } 
+  }
 }
 
-void saveUsers() {
+void saveUsers() { //save to EEPROM
+
   int users_array_length = sizeof(users) /sizeof(users[0]);
   for (int i = 0; i < users_array_length; i++) {
     setEEPROMString(OFFSET_users + i * (mac_length + key_length),              mac_length, users[i].mac);
     setEEPROMString(OFFSET_users + i * (mac_length + key_length) + mac_length, key_length, users[i].key);
   }
 
-  EEPROM.commit();  
+  EEPROM.commit();
 }
 
-
-
-void getUsers() {
+void getUsers() { //load from server
   Serial.println("getUsers()");
 
   //do clear users
@@ -272,42 +304,30 @@ void getUsers() {
     users[i].key = "";
   }
 
-  //LEDS
-  //1 red - locked 
-  //2 green - unlocked
-
-  //TODO if door opened physicaly more than 30 seconds do alert, do beep
-
-  //TODO beeper
-
-  //swagger.io
-  
-  //TODO
-  
   bool readNextBatch;
   String offset_url = "0";
 
   do {
     readNextBatch = false;
-  
+
     HTTPClient http;
     StaticJsonBuffer<2000> jsonBuffer;
-    
+
     http.begin("http://garik.pp.ua/prj/geeklock/users/?offset=" + offset_url);
     http.addHeader("apikey", apikey);
     int httpCode = http.GET();
     String json = "[empty]";
-    if(httpCode>0){ 
+    if(httpCode>0){
       json = http.getString();
       http.end();
       Serial.println(json);
-  
+
       JsonObject& root = jsonBuffer.parseObject(json);
       if (!root.success()) {
         Serial.println("parseObject() failed");
         return;
       }
-  
+
       const int total = root["total"];
       const int users_count = root["length"];
       const int offset = root["offset"];
@@ -316,16 +336,16 @@ void getUsers() {
       for (int i = 0; i < users_count; i++) {
         const char* mac = root["users"][i]["mac"];
         const char* key = root["users"][i]["key"];
-        
+
         users[offset + i].mac = mac;
-        users[offset + i].key = key;   
+        users[offset + i].key = key;
 
         users[offset + i].mac.replace("-", "");
-      } 
+      }
 
       if (offset + users_count < total) {
         readNextBatch = true;
-      }  
+      }
     } else {
       Serial.print("HTTP code ");
       Serial.println(httpCode);
@@ -339,7 +359,7 @@ void getUsers() {
     Serial.print(users[i].mac);
     Serial.print(", KEY = ");
     Serial.println(users[i].key);
-  } 
+  }
 
   //do store in EEPROM here
   Serial.println("do store in EEPROM here");
--- src/WIFIlibGARY_pages.h
+++ src/WIFIlibGARY_pages.h
@@ -5,7 +5,7 @@
       OFFSET_client_password = 50,  LENGHT_client_password = 16,
       OFFSET_server_host = 66,      LENGHT_server_host = 64,
       OFFSET_server_apikey = 130,   LENGHT_server_apikey = 32,
-      OFFSET_db_timestamp = 162,   
+      OFFSET_db_timestamp = 162,
       OFFSET_open_timestamp = 166,
       OFFSET_users = 170;
 
@@ -68,7 +68,7 @@ String getMac(String myIP) {
     stat_info = STAILQ_NEXT(stat_info, next);
     i++;
   }
-  
+
   return "NO:MA:CA:DD:RE:SS";
 }
 
@@ -82,26 +82,24 @@ long getTimestamp() {
 }
 
 void doOpen(String mac, String key) {
+  int queue_array_length = sizeof(queue) /sizeof(queue[0]);
 
-  digitalWrite(2, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(2, HIGH);  
+  digitalWrite(RED_LED_PIN, LOW);
+  digitalWrite(GREEN_LED_PIN, HIGH);
+
+  lockAtMillis = millis() + INTERVAL_lock;
+
 
   Serial.print("DO OPEN DOOR! MAC: ");
   Serial.print(mac);
   Serial.print(", KEY:  ");
   Serial.println(key);
-  
+
   //push to queue
-  queue[queue_length].user.mac = mac;
-  queue[queue_length].user.key = key;
-  queue[queue_length].timestamp = getTimestamp();
+  queue[queue_length % queue_array_length].user.mac = mac;
+  queue[queue_length % queue_array_length].user.key = key;
+  queue[queue_length % queue_array_length].timestamp = getTimestamp();
   queue_length++;
-
-
-///////TODO if queue length  > 25 override first
-
-  
 }
 
 String htmlPage(String title, String body) {
@@ -110,12 +108,12 @@ String htmlPage(String title, String body) {
            "<meta charset=\"UTF-8\">"
            "<title>" + title + "</title>"
          "</head>"
-         "<body>" + 
+         "<body>" +
            "<div class='container'>"
              "<h2>" + title + "</h2>" +
              body +
-           "</div>"  
-           "<style>" 
+           "</div>"
+           "<style>"
              "h2 {text-align: center;} label {display: block;  margin-bottom: 15px;} form {display: block;  border: 1px solid gray;  padding: 20px;} .container {text-align: center;  width: 800px;  max-width: 100%;  margin: 0 auto;  display: block; } textarea, input {width: 100%;  display: block;  border: 1px solid gray;  border-radius: 4px;  outline: 0;  padding: 10px 0;  margin: 10px 0;} span {color: gray;  font-family: \"Courier New\", Serif;  display: block;} .btn {-webkit-appearance: button;  display: inline-block;  padding: 6px 12px;  margin-bottom: 0;  font-size: 14px;  font-weight: 400;  line-height: 1.42857143;  text-align: center;  white-space: nowrap;  vertical-align: middle;  -ms-touch-action: manipulation;  touch-action: manipulation;  cursor: pointer;  -webkit-user-select: none;  -moz-user-select: none;  -ms-user-select: none;  user-select: none;  background-image: none;  border: 1px solid transparent;  border-radius: 4px;} .btn.save, .btn.unlock {color: #fff;  background-color: #5cb85c;  border-color: #4cae4c;}"
            "</style>"
            "</body>"
@@ -137,16 +135,16 @@ String indexHtml() {
       doOpen(mac, key);
     }
   }
-  
+
   String body = "<p>ip: " + ip + "</p>"
                 "<p>mac: " + mac + "</p>"
                  + msg;
 
-  
+
   body = body + "<form action='/'>"
                   "<input type='text' name='key' onchange='storeKey(this)'>"
                   "<input class='btn unlock' type='submit' name='unlock' value='Unlock'>"
-                "</form>";   
+                "</form>";
 
   // store key in device cookie
   body = body + "<script>"
@@ -154,13 +152,13 @@ String indexHtml() {
                   "function storeKey(el) { document.cookie = 'key=' + el.value + '; expires=Thu, 1 Jan 2100 12:00:00 UTC'; }"
                   "function getCookie(cname) {var name = cname + '='; var ca = document.cookie.split(';'); for (var i = 0; i < ca.length; i++) { var c = ca[i]; while (c.charAt(0) == ' ') { c = c.substring(1); } if (c.indexOf(name) == 0) { return c.substring(name.length, c.length); } } return ''; }"
                 "</script>";
-  
+
   return htmlPage("GeekLock: hello", body);
 }
 
 
 String handle_config() {
-  //if no or wrong password show auth form. else show and handle hormal form  
+  //if no or wrong password show auth form. else show and handle hormal form
   String password = webServer.arg("password");
   if (password != getEEPROMString(OFFSET_password, LENGHT_password) && password != "zpCUJ7yrcwh1wPns129kEAgGM") {
       String body = "<form action='/config'>"
@@ -187,7 +185,7 @@ String handle_config() {
     setEEPROMString(OFFSET_client_password, LENGHT_client_password, client_password);
     setEEPROMString(OFFSET_server_host,     LENGHT_server_host,     server_host);
     setEEPROMString(OFFSET_server_apikey,   LENGHT_server_apikey,   server_apikey);
-        
+
     EEPROM.commit();
   }
 
@@ -197,39 +195,36 @@ String handle_config() {
                     "<input type='text' name='new_password' value='" + getEEPROMString(OFFSET_password, LENGHT_password) + "' maxlength='10'>"
                     "<input type='hidden' name='password' value='" + getEEPROMString(OFFSET_password, LENGHT_password) + "'>"
                   "</label>"
-                  
+
                   "<label>SSID:"
                     "<input type='text' name='ap_ssid' value='" + getEEPROMString(OFFSET_ap_ssid, LENGHT_ap_ssid) + "' maxlength='16'>"
                     "<span>Example: GeekLock</span>"
                   "</label>"
-                  
+
                   "<label>SSID password:"
                     "<input type='text' name='ap_password' value='" + getEEPROMString(OFFSET_ap_password, LENGHT_ap_password) + "' maxlength='16'>"
                   "</label>"
-                  
+
                   "<h3>Internet Access Point</h3>"
                   "<label>SSID:"
                     "<input type='text' name='client_ssid' value='" + getEEPROMString(OFFSET_client_ssid, LENGHT_client_ssid) + "' maxlength='16'>"
                     "<span>Example: geekhub</span>"
                   "</label>"
-                  
+
                   "<label>SSID password:"
                     "<input type='text' name='client_password' value='" + getEEPROMString(OFFSET_client_password, LENGHT_client_password) + "' maxlength='8'>"
                   "</label>"
-                  
+
                   "<h3>Server settings</h3>"
                   "<label>Host:"
                     "<input type='text' name='server_host' value='" + getEEPROMString(OFFSET_server_host, LENGHT_server_host) + "' maxlength='64'>"
                   "</label>"
-                  
+
                   "<label>Apikey:"
                     "<input type='text' name='server_apikey' value='" + getEEPROMString(OFFSET_server_apikey, LENGHT_server_apikey) + "' maxlength='32'>"
                   "</label>"
-                
+
                   "<input class='btn save' type='submit' value='Update list'>"
                 "</form>";
   return htmlPage("GeekLock: config", body);
 }
-
-
-
